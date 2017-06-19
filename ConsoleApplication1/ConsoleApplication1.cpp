ee62cf6de4a0158ae3c56e23f28ae1ccee863621
lib <lib@PC-03>
2017-06-19 08:57:22 +0800

    引用型成员

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,9 +3,31 @@
 
 #include "stdafx.h"
 
+using namespace std;
 
-int main()
-{
+// 引用型成员的初始化问题
+
+class A {
+public:
+	int & rn;
+	int const N;
+	int * pn;
+	
+//	A(int & rn) :rn(rn) { cout << "in my ctor"; };
+	A(int & rn,int *pn=NULL,int n=0) :rn(rn),pn(pn),N(n) { cout << "in my ctor\n"; };
+
+};
+int gn;
+
+// A a;	// 在存在引用成员的情况下，无法生成无参构造函数，A::A(void)不存在。
+// A a={}; 错，理由同上
+
+
+A aa ={gn};
+A a = {gn, &gn};
+
+int main(){
+	cout << "!";
     return 0;
 }
 
