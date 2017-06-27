76783e1b45deb7ed02525e7792cddf05141f98ad
lib <lib@PC-03>
2017-06-27 16:41:09 +0800

    友元可以访问protected成员

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,12 +5,22 @@
 
 using namespace std;
 
-// 引用型成员的初始化问题
 
-int gn;
-int *pn = NULL;	// 依然只有 NULL 宏，并没有“null”保留字，别和 JavaScript 混了
+
+class A {
+protected:
+	int n;
+	friend void fn(A *pa);	// 友元
+};
+void fn(A *pa) {
+	pa->n = 10;	// 可以访问 protected 成员
+	cout << "赋值 proteced 成员";
+}
 
 int main(){
+	A a;
+	fn(&a);
+
 	cout << "!";
     return 0;
 }
