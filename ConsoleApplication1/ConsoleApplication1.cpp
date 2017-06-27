fab5ec09c697a9eb2c9eb6453adbcf0d644c9b6d
lib <lib@PC-03>
2017-06-27 13:57:25 +0800

    保留字中依然没有“null”

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,24 +7,8 @@ using namespace std;
 
 // 引用型成员的初始化问题
 
-class A {
-public:
-	int & rn;
-	int const N;
-	int * pn;
-	
-//	A(int & rn) :rn(rn) { cout << "in my ctor"; };
-	A(int & rn,int *pn=NULL,int n=0) :rn(rn),pn(pn),N(n) { cout << "in my ctor\n"; };
-
-};
 int gn;
-
-// A a;	// 在存在引用成员的情况下，无法生成无参构造函数，A::A(void)不存在。
-// A a={}; 错，理由同上
-
-
-A aa ={gn};
-A a = {gn, &gn};
+int *pn = NULL;	// 依然只有 NULL 宏，并没有“null”保留字，别和 JavaScript 混了
 
 int main(){
 	cout << "!";
