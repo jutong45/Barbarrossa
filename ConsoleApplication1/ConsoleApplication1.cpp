87ed21d01b6720a7cb32fb5ecc28bcf182878478
lib <lib@PC-03>
2017-06-28 11:00:47 +0800

    对象数组初始化

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,21 +6,20 @@
 using namespace std;
 
 
+// 对象数组初始化
 
 class A {
-protected:
 	int n;
-	friend void fn(A *pa);	// 友元
+public:
+//	A() {};
+	A (int n,int m) :n(n){};	// ctor
 };
-void fn(A *pa) {
-	pa->n = 10;	// 可以访问 protected 成员
-	cout << "赋值 proteced 成员";
-}
+
 
 int main(){
-	A a;
-	fn(&a);
+	A *pa = new A[]{A(1,2), A(1,3), A(3,3)};	// 堆数组初始化，与栈数组的语法有区别
 
+	delete ((A*)0);	// 可以删除空指针，效果等于空语句
 	cout << "!";
     return 0;
 }
