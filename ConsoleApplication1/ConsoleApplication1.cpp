da99839fab54d12d94e1d9bc9870c88ef67b17e8
lib <lib@PC-03>
2017-06-30 14:19:51 +0800

    函数引用

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,29 +5,14 @@
 
 using namespace std;
 
+// 函数引用
 
-// operator++()
-
-class Point {
-public:
-	int x;
-	int y;
-	Point& operator ++() {
-		this->x++;
-		this->y++;
-
-		cout <<"x:" <<this->x <<"\t y:" << this -> y <<"\n";
-		return (*this);
-	}
-};
-void Show(Point &p) {
-	cout << "x:" << p.x << "\t y:" << p.y <<"\n";
+void Show() {
+	cout << "hi\n";
 }
 int main() {
-	int n = 5;
-	cout << ++n++ << "\n";	// 错误语句，因为 n++ 的值不是左值，不能作为 ++n 的操作数
-
-	Point pixel = {3,9};
-	Show(pixel);
-	Show(++pixel);
+	void(*pfn)() =Show;
+	void (&rfn) () = Show;
+	pfn();
+	rfn();
 }
