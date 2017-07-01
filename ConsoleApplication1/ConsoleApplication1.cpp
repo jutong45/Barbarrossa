f262c11ade61808cbf3c42d8af9e2cd8ee0142a1
lib <lib@PC-03>
2017-07-01 15:56:41 +0800

    一维数组转型为二维

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,14 +5,12 @@
 
 using namespace std;
 
-// 函数引用
+// 一维数组转型为二维
+int a[10] = { 0,1,2,3,4,5,6 };	// 一维数组
 
-void Show() {
-	cout << "hi\n";
+void fn(int a[][5]) {		// 形参是二维数组（实际退化为指向一维数组的指针）
+	cout << a[0][1];
 }
 int main() {
-	void(*pfn)() =Show;
-	void (&rfn) () = Show;
-	pfn();
-	rfn();
+	fn((int (*)[5])a);	// 将一维数组转型为二维传递给函数
 }
