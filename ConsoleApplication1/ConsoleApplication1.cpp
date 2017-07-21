5fce10a8514f2b2e562eeb8f90e323c583199aa4
lib <lib@PC-03>
2017-07-21 15:37:46 +0800

    突破语言限制，可指定命名参数的模板

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,24 +6,62 @@
 using namespace std;
 using std::string;
 
-template <int N=8>
-class B {
-	// static int n=N;
+// 命名模板参数功能：匹配模板参数时，不是按照模板实参的位置，而是根据提供的名字
+
+// 首先，声明一个类，用来存放所有默认值，供派生类覆盖
+class AllDefaultParam {	// 保存所有备选默认值
 public:
-	B() {
-		cout << "B's ctor\n";
-	}
+	typedef int SKY;
+	typedef char EARTH;
+	typedef double HUMAN;
 };
 
-template <template <int>class T,int N=10>
-class D :public T<N> {
+// 然后，为每个模板参数都构造一个用来修改默认值的类模板。它们派生自默认值类，通过覆盖基类的成员类型的方式，将一个新值替换掉对应的默认值
+template <typename T>
+class Sky_is :public virtual AllDefaultParam {	// 用于覆盖基类中的同名方法
 public:
-	D():T<N>() {
-		cout << "the ctor of D \n";
-	};
+	typedef T SKY;
 };
-// template void A<int>::fn<int>(int,int);	// 显式实例化
+template <typename T>
+class Earth_is :public virtual AllDefaultParam {
+public:
+	typedef T EARTH;
+};
+template<typename T>
+class Human_is :public virtual AllDefaultParam {
+public:
+	typedef T HUMAN;
+};
+
+// 由于每替换一个默认值就需要继承一个默认值类，而任何类只能直接继承一次，所以需要让一个类型变成不同类型
+template <typename T, int Luanshu>
+class LuanshuBaozhuang :public virtual T {	// 用乱数包装一个类，用以避免直接基类冲突。用不同乱数所生产的类型其实完全相同，但却被视为不同类型。
+};
+
+// 通过这个整合成完整参数表 
+template <typename SKY, typename EARTH, typename HUMAN>	
+class Param :public SKY,public EARTH,public HUMAN {	// 用于合成完整实参
+};
+
+
+// 下面是两种用户使用该机制的代码，1是我们的版本，2是《Template》的版本
+template <typename SKY= LuanshuBaozhuang<AllDefaultParam, 1>, 
+		  typename EARTH= LuanshuBaozhuang<AllDefaultParam, 33>, 
+		  typename HUMAN= LuanshuBaozhuang<AllDefaultParam, 555>>
+class UserClass :public Param<SKY,EARTH,HUMAN> {	// 用户类
+};
+
+template <typename Sky = LuanshuBaozhuang<AllDefaultParam, 1>,
+	typename Earth = LuanshuBaozhuang<AllDefaultParam, 33>,
+	typename Human = LuanshuBaozhuang<AllDefaultParam, 555>>
+class UserClass2 {	// 用户类
+public:
+	typedef  Param<Sky, Earth, Human> Arg;
+};
+
 
 int main() {
-	D<B> *pD = new D<B>;
+	cout << typeid(UserClass<Earth_is<char *>>::EARTH).name();	// 写法简单，但采用继承方式会导致内存开销
+
+	cout << typeid(UserClass2<Earth_is<char *>>::Arg::EARTH).name();	// 写法虽繁琐，但类内无垃圾开销
 }
